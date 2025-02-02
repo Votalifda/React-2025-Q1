426c0fea1130a3f4c5be53d785c0ba7a52aaf0e5
Denis Filatov <logos.fda@gmail.com>
2025-02-02 22:34:12 +0200

    feat: add main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { ChangeEvent, Component } from 'react';
+import Table from './parts/Table.tsx';
+import Loader from './parts/Loader.tsx';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface AppState {
+  isLoading: boolean;
+  search: string;
+  items: Array<{
+    name: string;
+    gender: string;
+    birth_year: string;
+  }>;
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component<Record<string, never>, AppState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      search: localStorage.getItem('search') || '',
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchEpisodes().then();
+  }
+
+  fetchEpisodes = async () => {
+    try {
+      this.setState({ ...this.state, isLoading: true });
+      const response = await fetch(
+        `https://swapi.dev/api/people?search=${this.state.search.trim()}`
+      );
+      if (!response.ok) {
+        throw new Error('Network response error');
+      }
+      const data = await response.json();
+      const items: Array<{ name: string; gender: string; birth_year: string }> =
+        data?.results || [];
+      this.setState({
+        ...this.state,
+        isLoading: false,
+        items: items.map((item) => ({
+          name: item.name,
+          gender: item.gender,
+          birth_year: item.birth_year,
+        })),
+      });
+    } catch (error) {
+      this.setState({ ...this.state, isLoading: false });
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  onSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState(() => ({
+      ...this.state,
+      search: e.target.value,
+    }));
+  };
+  handleOnSearch = () => {
+    localStorage.setItem('search', this.state.search);
+    this.fetchEpisodes();
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="section small">
+          <label>Search:</label>
+          <input
+            onChange={(e) => this.onSearchInputChange(e)}
+            value={this.state.search}
+          />
+          <button className="btnSearch" onClick={this.handleOnSearch}>
+            Search
+          </button>
+        </div>
+        <div className="section big">
+          {this.state.isLoading ? (
+            <Loader />
+          ) : (
+            <Table items={this.state.items} />
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
