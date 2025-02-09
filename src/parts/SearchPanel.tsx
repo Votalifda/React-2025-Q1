64cd3bc9d4c44420194d5abc1783d5c2d42a5cf1
Denis Filatov <logos.fda@gmail.com>
2025-02-10 00:24:07 +0200

    feat: add 404 page

--- src/parts/SearchPanel.tsx
+++ src/parts/SearchPanel.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, FC } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import ErrorBoundary from '../ErrorBoundary.tsx';
 import ThrowError from './ThrowError.tsx';
 import '../App.css';
@@ -10,12 +11,18 @@ interface Props {
 }
 
 const SearchPanel: FC<Props> = ({ search, setSearchValue, handleOnSearch }) => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
   const onSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
 
+  const handleClose = () => {
+    navigate(`/?${searchParams.toString()}`);
+  };
+
   return (
-    <div className="section small">
+    <div className="section small" onClick={handleClose}>
       <label>Search:</label>
       <input onChange={(e) => onSearchInputChange(e)} value={search} />
       <button className="btnSearch" onClick={handleOnSearch}>
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import CardDetails from './parts/CardDetails.tsx';
+import Page404 from './Page404.tsx';
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Route path="/" element={<App />}>
         <Route path="details/:id" element={<CardDetails />} />
       </Route>
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 };
