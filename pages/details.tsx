e9b7033f6edf5b864eb38128b3d9385e4ba019a9
Denis Filatov <logos.fda@gmail.com>
2025-03-09 21:16:15 +0200

    feat: refactor

---
+++ pages/details.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Details</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <main>
+          Details
+        </main>
+      </div>
+    </>
+  );
+}
